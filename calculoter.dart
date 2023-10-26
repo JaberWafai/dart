59a1e51b65cb18af4ee5981bf952c83c8e3bfaff
JaberWafai <93190643+JaberWafai@users.noreply.github.com>
2023-10-26 21:40:44 +0300

    calculoter.dart

--- calculoter.dart
+++ calculoter.dart
@@ -1,9 +1,10 @@
+library globals;
+
 import 'dart:io';
 
+bool globalflag = true;
 void main() {
-  bool flag = true;
-
-  while (flag) {
+  while (globalflag) {
     print("Enter operation:");
     print("1- +");
     print("2- -");
@@ -11,52 +12,17 @@ void main() {
     print("4- /");
     print("5 for exit the program!");
 
-    int operationType = int.parse(stdin.readLineSync()!);
-
-    if (operationType == 5) {
-      flag = false;
-      print("Good Bye");
-      break;
-    }
-
-    if (operationType < 1 || operationType > 5) {
-      print("Wrong choice! Please try again.");
-      continue;
-    }
-
-    print("Enter the first number:");
-    double num1 = double.parse(stdin.readLineSync()!);
-    print("Enter the second number:");
-    double num2 = double.parse(stdin.readLineSync()!);
-
-    double result = 0;
-
-    switch (operationType) {
-      case 1:
-        result = sum(num1, num2);
-        break;
-      case 2:
-        result = sub(num1, num2);
-        break;
-      case 3:
-        result = mul(num1, num2);
-        break;
-      case 4:
-        result = div(num1, num2);
-        break;
-    }
-
-    print("The result is ${result}");
+    String? operationType = stdin.readLineSync()!;
+    calculator(operationType: operationType);
   }
 }
 
-double sum(num1, num2) {
-  return num1 + num2;
+double sum(double num1, [double num2 = 0]) {
+  double sum = num1 + num2;
+  return sum;
 }
 
-double sub(num1, num2) {
-  return num1 - num2;
-}
+double sub(num1, num2) =>  num1 - num2;
 
 double mul(num1, num2) {
   return num1 * num2;
@@ -68,4 +34,38 @@ double div(num1, num2) {
     return double.nan;
   } else
     return num1 / num2;
+}
+
+void calculator({required String operationType}) {
+  if (operationType == '5') {
+    globalflag = false;
+    print("Good Bye!");
+  } else {
+    print("Enter the first number:");
+    double num1 = double.parse(stdin.readLineSync()!);
+    print("Enter the second number:");
+    var num2 = double.tryParse(stdin.readLineSync()!);
+
+    double result = 0;
+
+    switch (operationType) {
+      case '1':
+        if (num2 == null) {
+          result = sum(num1);
+        } else {
+          result = sum(num1, num2);
+        }
+        break;
+      case '2':
+        result = sub(num1, num2);
+        break;
+      case '3':
+        result = mul(num1, num2);
+        break;
+      case '4':
+        result = div(num1, num2);
+        break;
+    }
+    print("The result is ${result}");
+  }
 }
